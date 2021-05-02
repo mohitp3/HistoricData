beca1965d81813fb5d8c508c645a0e2a3f76703a
Mohit Pandey <mht.pdy@gmail.com>
2021-05-02 19:17:04 +0530

    Update controller.js

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -23,7 +23,7 @@ exports.create = (req,res)=>{
     user
         .save(user)
         .then(data => {
-            res.send(data);
+            //res.send(data);
             res.redirect('/');
         })
         .catch(err =>{
